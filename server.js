daec31beebf26a280e3ec5974e15ea3769679985
Ingsha Hang Subba <68878902+ingsha09@users.noreply.github.com>
2025-06-29 13:30:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -695,7 +695,11 @@ app.post('/api/download-image', async (req, res) => {
   try {
     browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage'
+      ]
     });
     const page = await browser.newPage();
 
@@ -732,7 +736,11 @@ app.post('/api/download-pdf', async (req, res) => {
   try {
     browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage'
+      ]
     });
     const page = await browser.newPage();
 
